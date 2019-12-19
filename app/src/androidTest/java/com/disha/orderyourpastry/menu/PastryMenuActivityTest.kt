957bc067c0273e116a63f11e4a6f8e3cab1a7a3b
disham <disha.m@peopleinteractive.in>
2019-12-19 19:06:21 +0530

    Exercise2_test_recyclerview_item_click_opens_order_activity

--- app/src/androidTest/java/com/disha/orderyourpastry/menu/PastryMenuActivityTest.kt
+++ app/src/androidTest/java/com/disha/orderyourpastry/menu/PastryMenuActivityTest.kt
@@ -1,37 +1,11 @@
 package com.disha.orderyourpastry.menu
 
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.disha.orderyourpastry.R
-import com.disha.orderyourpastry.order.PastryOrderActivity
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.Assert.*
 
-@RunWith(AndroidJUnit4::class)
-class PastryMenuActivityTest {
+//TODO: run with
+class PastryMenuActivityTest{
 
-    @get:Rule
-    val intentTestRule: IntentsTestRule<PastryMenuActivity> =
-        IntentsTestRule(PastryMenuActivity::class.java)
+    //TODO: add rule
 
-    @Test
-    fun onClickOfPastry_OrderActivityOpens() {
-        Espresso.onView(ViewMatchers.withId(R.id.rv_pastries))
-            .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                    0,
-                    ViewActions.click()
-                )
-            )
-        intended(hasComponent(PastryOrderActivity::class.java.name))
-    }
-
+    //TODO: add test to check: clicking on recyclerView item opens PastryOrderActivity
 }
