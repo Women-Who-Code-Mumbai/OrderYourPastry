6d7ca0aa6408c4ed7fde9be8c18673d959299097
disham <disha.m@peopleinteractive.in>
2019-12-19 18:57:50 +0530

    Exercise4_test_recyclerview_items

---
+++ app/src/androidTest/java/com/disha/orderyourpastry/menu/DummyData.kt
@@ -0,0 +1,5 @@
+package com.disha.orderyourpastry.menu
+
+object DummyData {
+    //TODO: provide dummy data for test
+}
---
+++ app/src/androidTest/java/com/disha/orderyourpastry/menu/PastryMenuActivityTest2.kt
@@ -0,0 +1,13 @@
+package com.disha.orderyourpastry.menu
+
+import org.junit.Assert.*
+
+//TODO: add run with
+class PastryMenuActivityTest2{
+
+    //TODO: add activity test rule
+
+    //TODO: add test to check items count on recycler view
+
+    //TODO: add test to check set item name matches the data passed item name
+}
