a644d75d912d65ff44da81811deb7b6fdc45df31
disham <disha.m@peopleinteractive.in>
2019-12-19 18:52:52 +0530

    Exercise3_summary_send_mail_intent

---
+++ app/src/androidTest/java/com/disha/orderyourpastry/summary/OrderSummaryActivityTest.kt
@@ -0,0 +1,11 @@
+package com.disha.orderyourpastry.summary
+
+import org.junit.Assert.*
+
+//TODO: add run with
+class OrderSummaryActivityTest{
+
+    //TODO: add rule
+
+    //TODO: add test which will check: send mail opens intent with desired action
+}
