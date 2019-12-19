268c3c0333b1f85524ac61b294d36d3843ce99f6
disham <disha.m@peopleinteractive.in>
2019-12-19 18:37:55 +0530

    exercise1_test_reduce_quantity_button_click

---
+++ app/src/androidTest/java/com/disha/orderyourpastry/order/PastryOrderActivityTest.kt
@@ -0,0 +1,14 @@
+package com.disha.orderyourpastry.order
+
+import org.junit.Assert.*
+
+// TODO: Add annotation to specify AndroidJUnitRunner
+class PastryOrderActivityTest {
+
+    //TODO: Add the rule
+
+    //TODO: Write a test which will:
+    // Check the quantity is 0,
+    // click on reduce quantity button,
+    // check the reduce button do not decrement the quantity and price
+}
