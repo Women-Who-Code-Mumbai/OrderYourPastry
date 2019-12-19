5d8ac883e7ce926472cd056bb83c2e01217f9db6
disham <disha.m@peopleinteractive.in>
2019-12-19 18:39:41 +0530

    solution1_test_reduce_quantity_button_click

--- app/src/androidTest/java/com/disha/orderyourpastry/order/PastryOrderActivityTest.kt
+++ app/src/androidTest/java/com/disha/orderyourpastry/order/PastryOrderActivityTest.kt
@@ -1,14 +1,38 @@
 package com.disha.orderyourpastry.order
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.disha.orderyourpastry.R
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-// TODO: Add annotation to specify AndroidJUnitRunner
+@RunWith(AndroidJUnit4::class)
 class PastryOrderActivityTest {
 
-    //TODO: Add the rule
+    @get:Rule
+    var mActivityTestRule: ActivityTestRule<PastryOrderActivity?>? =
+        ActivityTestRule(PastryOrderActivity::class.java)
 
-    //TODO: Write a test which will:
-    // Check the quantity is 0,
-    // click on reduce quantity button,
-    // check the reduce button do not decrement the quantity and price
+    @Test
+    fun clickDecrementButton_ChangesQuantityAndCost() {
+
+        val totalPrice = 0F
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_total_quantity))
+            .check(ViewAssertions.matches(ViewMatchers.withText("0")))
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_remove)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_total_quantity))
+            .check(ViewAssertions.matches(ViewMatchers.withText("0")))
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_total_amount))
+            .check(ViewAssertions.matches(ViewMatchers.withText(totalPrice.toString())))
+    }
 }
