212361b6d89ae3fea6e0cae7a0ec4607583ec8a4
disham <disha.m@peopleinteractive.in>
2019-12-19 18:45:16 +0530

    Exercise2_test_recyclerview_item_click_opens_order_activity

---
+++ app/src/androidTest/java/com/disha/orderyourpastry/menu/PastryMenuActivityTest.kt
@@ -0,0 +1,11 @@
+package com.disha.orderyourpastry.menu
+
+import org.junit.Assert.*
+
+//TODO: run with
+class PastryMenuActivityTest{
+
+    //TODO: add rule
+
+    //TODO: add test to check: clicking on recyclerView item opens PastryOrderActivity
+}
